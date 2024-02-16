e956430f14527c8ea3f09e63827e3f0b2cef806f
Jon Fairbanks <fairplay89@gmail.com>
2024-02-15 22:19:05 -0800

    Update Page Title

--- components/page_config.py
+++ components/page_config.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def set_page_config():
     st.set_page_config(
-        page_title='Offline, Open-Source RAG',
+        page_title='Local RAG',
         page_icon='📚',
         layout='wide',
         initial_sidebar_state='expanded'
