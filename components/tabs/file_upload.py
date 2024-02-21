a9ac1458684aae137780510b123fab3502d3229d
Jon Fairbanks <fairplay89@gmail.com>
2024-02-21 02:46:23 -0800

    RIP nested expanders

--- components/tabs/file_upload.py
+++ components/tabs/file_upload.py
@@ -12,115 +12,94 @@ def file_upload():
     st.caption("Convert your files to embeddings for utilization during chat")
     st.write("")
 
-    with st.expander("Local Files", expanded=True):
-        if st.session_state["selected_model"] is not None:
-            uploaded_files = st.file_uploader(
-                "Select Files",
-                accept_multiple_files=True,
-                type=(
-                    "csv",
-                    "docx",
-                    "epub",
-                    "ipynb",
-                    "json",
-                    "md",
-                    "pdf",
-                    "ppt",
-                    "pptx",
-                ),
-            )
-        else:
-            st.warning("Please configure Ollama settings before proceeding!", icon="⚠️")
-            uploaded_files = st.file_uploader(
-                "Select Files",
-                accept_multiple_files=True,
-                type=(
-                    "csv",
-                    "docx",
-                    "epub",
-                    "ipynb",
-                    "json",
-                    "md",
-                    "pdf",
-                    "ppt",
-                    "pptx",
-                ),
-                disabled=True,
-            )
+    if st.session_state["selected_model"] is not None:
+        uploaded_files = st.file_uploader(
+            "Select Files",
+            accept_multiple_files=True,
+            type=("csv", "docx", "epub", "ipynb", "json", "md", "pdf", "ppt", "pptx",),
+        )
+    else:
+        st.warning("Please configure Ollama settings before proceeding!", icon="⚠️")
+        uploaded_files = st.file_uploader(
+            "Select Files",
+            accept_multiple_files=True,
+            type=("csv", "docx", "epub", "ipynb", "json", "md", "pdf", "ppt", "pptx",),
+            disabled=True,
+        )
 
-        if len(uploaded_files) > 0:
-            st.session_state["file_list"] = uploaded_files
+    if len(uploaded_files) > 0:
+        st.session_state["file_list"] = uploaded_files
 
-            with st.status("Preparing your data...", expanded=True) as status:
-                error = None
+        with st.status("Preparing your data...", expanded=True) as status:
+            error = None
 
-                ######################
-                # Save Files to Disk #
-                ######################
+            ######################
+            # Save Files to Disk #
+            ######################
 
-                st.caption("Uploading Files Locally")
-                for uploaded_file in uploaded_files:
-                    with st.spinner(f"Processing {uploaded_file.name}..."):
-                        save_dir = os.getcwd() + "/data"
-                        func.save_uploaded_file(uploaded_file, save_dir)
+            st.caption("Uploading Files Locally")
+            for uploaded_file in uploaded_files:
+                with st.spinner(f"Processing {uploaded_file.name}..."):
+                    save_dir = os.getcwd() + "/data"
+                    func.save_uploaded_file(uploaded_file, save_dir)
 
-                st.caption("Loading Embedding Model")
+            st.caption("Loading Embedding Model")
 
-                ######################################
-                # Create Llama-Index service-context #
-                # to use local LLMs and embeddings   #
-                ######################################
+            ######################################
+            # Create Llama-Index service-context #
+            # to use local LLMs and embeddings   #
+            ######################################
 
-                try:
-                    llm = ollama.create_ollama_llm(
-                        st.session_state["selected_model"],
-                        st.session_state["ollama_endpoint"],
-                    )
-                    # resp = llm.complete("Hello!")
-                    # print(resp)
-                    service_context = llama_index.create_service_context(llm)
-                except Exception as err:
-                    print(f"Setting up Service Context failed: {err}")
-                    error = err
+            try:
+                llm = ollama.create_ollama_llm(
+                    st.session_state["selected_model"],
+                    st.session_state["ollama_endpoint"],
+                )
+                # resp = llm.complete("Hello!")
+                # print(resp)
+                service_context = llama_index.create_service_context(llm)
+            except Exception as err:
+                print(f"Setting up Service Context failed: {err}")
+                error = err
 
-                #######################################
-                # Load files from the data/ directory #
-                #######################################
+            #######################################
+            # Load files from the data/ directory #
+            #######################################
 
-                st.caption("Processing File Data")
-                try:
-                    documents = llama_index.load_documents(save_dir)
-                    st.session_state["documents"] = documents
-                except Exception as err:
-                    print(f"Document Load Error: {err}")
-                    error = err
+            st.caption("Processing File Data")
+            try:
+                documents = llama_index.load_documents(save_dir)
+                st.session_state["documents"] = documents
+            except Exception as err:
+                print(f"Document Load Error: {err}")
+                error = err
 
-                ###########################################
-                # Create an index from ingested documents #
-                ###########################################
+            ###########################################
+            # Create an index from ingested documents #
+            ###########################################
 
-                st.caption("Creating File Index")
-                try:
-                    llama_index.create_query_engine(documents, service_context)
-                except Exception as err:
-                    print(f"Index Creation Error: {err}")
-                    error = err
+            st.caption("Creating File Index")
+            try:
+                llama_index.create_query_engine(documents, service_context)
+            except Exception as err:
+                print(f"Index Creation Error: {err}")
+                error = err
 
-                #####################
-                # Show Final Status #
-                #####################
+            #####################
+            # Show Final Status #
+            #####################
 
-                if error is not None:
-                    status.update(
-                        label="File processing failed.", state="error", expanded=True,
-                    )
-                    st.error(error)
-                else:
-                    status.update(
-                        label="Your files are ready. Let's chat!",
-                        state="complete",
-                        expanded=False,
-                    )
+            if error is not None:
+                status.update(
+                    label="File processing failed.", state="error", expanded=True,
+                )
+                st.error(error)
+            else:
+                status.update(
+                    label="Your files are ready. Let's chat!",
+                    state="complete",
+                    expanded=False,
+                )
 
     with st.expander("GitHub Repo", expanded=False):
         st.write(":grey[Coming Soon&trade;]")
