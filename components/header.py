bcb08bda913273b5036e715e36c856b26ef894aa
Jon Fairbanks <fairplay89@gmail.com>
2024-02-21 02:34:56 -0800

    Disable header anchor

--- components/header.py
+++ components/header.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 
 def set_page_header():
-    st.header("📚 Offline, Open-Source RAG")
+    st.header("📚 Offline, Open-Source RAG", anchor=False)
     st.caption(
         "Ingest files for retrieval augmented generation (RAG) with open-source Large Language Models (LLMs), all without 3rd parties or sensitive data leaving your network."
     )
