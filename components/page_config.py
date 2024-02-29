7699fd7623ddd400a51e89ea9c8f47cae5b6843a
Jon Fairbanks <fairplay89@gmail.com>
2024-02-28 22:15:51 -0800

    State Controlled Sidebar

--- components/page_config.py
+++ components/page_config.py
@@ -6,5 +6,10 @@ def set_page_config():
         page_title="Local RAG",
         page_icon="📚",
         layout="wide",
-        initial_sidebar_state="expanded",
+        initial_sidebar_state=st.session_state['sidebar_state'],
+        menu_items={
+            'Get Help': 'https://github.com/jonfairbanks/local-rag/discussions',
+            'Report a bug': "https://github.com/jonfairbanks/local-rag/issues",
+            'About': None
+        }
     )
